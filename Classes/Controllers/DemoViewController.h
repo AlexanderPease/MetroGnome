67930df2ef8f4181006f229b22ad955657bc8cde
Alexander Pease <AlexanderPease@dynamic-oit-vapornet-c-3764.princeton.edu>
2012-02-05 16:52:49 -0500

    Kairos demo. Garbage. But it worked well!

--- Classes/Controllers/DemoViewController.h
+++ Classes/Controllers/DemoViewController.h
@@ -15,14 +15,21 @@
     MGSheetMusicView *_sheet;
     UILabel *_label;
     UIImageView *_logo;
+    UIImageView *_musicDisplay;
+    UIButton *_button;
+    UIButton *_playPause;
     int _progress;
 }
 @property(nonatomic,retain)MGSheetMusicView *sheet;
 @property(nonatomic,retain)UILabel *label;
 @property(nonatomic,retain)UIImageView *logo;
+@property(nonatomic,retain)UIImageView *musicDisplay;
+@property(nonatomic,retain)UIButton *button;
+@property(nonatomic,retain)UIButton *playPause;
 @property(readwrite,assign)int progress;
 
 -(void)go; //Starts demo
--(void)explain;
+-(void)explain; //Explains lesson
+-(void)background;//Teaches blues scale
 -(void)play;
 @end
--- Classes/Controllers/DemoViewController.m
+++ Classes/Controllers/DemoViewController.m
@@ -11,12 +11,26 @@
 #import "bassmidi.h"
 #import "MGInstrument.h"
 
+#define BUTTONWIDTH 150
+#define BUTTONHEIGHT 45
+#define LABELOFFSET 30
+
+@interface MGDemoViewController (Private)
+-(void)helpBackground;
+-(void)playBackground;
+-(void)lesson;
+-(void)playLesson;
+-(void)pauseLesson;
+@end
 
 
 @implementation MGDemoViewController
 @synthesize sheet = _sheet;
 @synthesize label = _label;
 @synthesize logo = _logo;
+@synthesize musicDisplay = _musicDisplay;
+@synthesize button = _button;
+@synthesize playPause = _playPause;
 @synthesize progress = _progress;
 
 -(void)nextScreen {
@@ -26,7 +40,11 @@
     }
     else if (self.progress == 1) {
         self.progress++;
-        [self play];
+        [self background];
+    }
+    else if (self.progress == 2) {
+        self.progress++;
+        [self lesson];
     }
 }
 -(void)go {
@@ -40,29 +58,187 @@
     [self.view addSubview:self.sheet];
     
     self.logo = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"TrebleClefLogoBold.png"]];
-    CGFloat heightOffset = 200.0;
+    CGFloat heightOffset = 240.0;
     self.logo.center = CGPointMake(self.sheet.frame.size.width/2,self.sheet.frame.size.height/2 - heightOffset);
     self.sheet.demo = self;
     [self.sheet addSubview:self.logo];
     
-    CGSize textSize = CGSizeMake(850.0, 200.0);
+    CGSize textSize = CGSizeMake(850.0, 500.0);
     self.label = [[UILabel alloc]initWithFrame:CGRectMake(0,0, 
                                                           textSize.width, textSize.height)]; 
-    self.label.center = CGPointMake(self.sheet.frame.size.width/2, self.sheet.frame.size.height/2);
+    int offset = 30;
+    self.label.center = CGPointMake(self.sheet.frame.size.width/2, self.sheet.frame.size.height/2-LABELOFFSET);
     self.label.numberOfLines = 4;
     self.label.text = [NSString stringWithFormat:@"Welcome to the 2012 Kairos Global Summit!\n"
                   "We have designed a short musical lesson for you to try.\n\n"
-                  "Please touch the screen to continue."];
+                  "Tap the screen to continue."];
     self.label.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.0]; //Clear background
     [self.label setFont:[UIFont fontWithName:@"Arial" size:32.0]];
     [self.sheet addSubview:self.label];
+    [self.label release];
 }
 
 -(void)explain {
-    int i = 0;
+    self.label.numberOfLines = 7;
+    self.label.text = [NSString stringWithFormat:@"Lesson: Repeat After Me(troGnome)\n"
+                       "Style: Blues\n\n"
+                       "The piano will play a short phrase every other measure.\n"
+                       "Your goal is to repeat each phrase in the next measure.\n"
+                       "Try to stay in time with MetroGnome's accompaniment!\n\n"
+                       ""];
+
+    
 }
 
--(void)play {
+-(void)background {
+    self.label.center = CGPointMake(self.sheet.frame.size.width/2, self.sheet.frame.size.height/2-LABELOFFSET);
+    self.label.text = [NSString stringWithFormat:@"Scale: Blues (Major)\n\n"
+                       "Many scales are associated with the blues. "
+                       "Below is a scale commonly used in the C Major Blues."
+                       "This lesson only uses the first three notes: C, Eflat, and F. \n\n"
+                       "If you need help finding these notes, tap the Help button."];
+
+    
+    [self.musicDisplay removeFromSuperview];
+    self.musicDisplay = [[UIImageView alloc]initWithImage:
+                         [UIImage imageNamed:@"C Major Blues Scale.tiff"]];
+    CGFloat scale = 0.7;
+    self.musicDisplay.frame = CGRectMake(0, 0, 
+                                         self.musicDisplay.frame.size.width*scale, 
+                                         self.musicDisplay.frame.size.height*scale);
+    self.musicDisplay.center = CGPointMake(self.sheet.frame.size.width/2, 
+                                           self.sheet.frame.size.height-self.musicDisplay.frame.size.height);
+    [self.sheet addSubview:self.musicDisplay];
+    [self.musicDisplay release];
+    
+    //Help button
+    self.button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    CGFloat margin = 10.0;
+    self.button.frame = CGRectMake(margin, margin, BUTTONWIDTH, BUTTONHEIGHT);
+    [self.button setFont:[UIFont fontWithName:@"Arial" size:30.0]];
+    [self.button setTitle:[NSString stringWithFormat:@"Help"] 
+                 forState:UIControlStateNormal];
+    [self.button setTitleColor:[UIColor blackColor] 
+                      forState:UIControlStateNormal];
+    //[self.button addTarget:self action:@selector(helpBackground) forControlEvents:UIControlEventTouchUpInside];
+    [self.sheet addSubview:self.button];
+    
+    
+    //Play/pause button
+    self.playPause = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [self.playPause setFont:[UIFont fontWithName:@"Arial" size:30.0]];
+    self.playPause.frame = CGRectMake(self.sheet.frame.size.width/2 - BUTTONWIDTH/2, 
+                                      self.sheet.frame.size.height-BUTTONHEIGHT-20, 
+                                      BUTTONWIDTH, BUTTONHEIGHT);
+    [self.playPause setTitle:[NSString stringWithFormat:@"Listen"] 
+                 forState:UIControlStateNormal];
+    [self.playPause setTitleColor:[UIColor blackColor] 
+                      forState:UIControlStateNormal];
+    [self.playPause addTarget:self action:@selector(playBackground) forControlEvents:UIControlEventTouchUpInside];
+    [self.sheet addSubview:self.playPause];
+        
+}
+
+
+-(void)helpBackground {
+    [self.button setTitle:[NSString stringWithFormat:@"Back"] 
+                 forState:UIControlStateNormal];
+    [self.button setTitleColor:[UIColor blackColor] 
+                      forState:UIControlStateNormal];
+    //add button action
+    [self.button addTarget:self action:@selector(background) forControlEvents:UIControlEventTouchUpInside];
+    
+    self.label.text = [NSString stringWithFormat:@"Notes: C, Eflat, F"];
+    self.label.center = CGPointMake(self.label.center.x, self.label.center.y-100);
+
+    
+    [self.musicDisplay removeFromSuperview];
+    self.musicDisplay =  [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Kairos3NotesandKeys.tiff"]];
+    CGFloat scale = 1.0;
+    self.musicDisplay.frame = CGRectMake(0, 0, 
+                                         self.musicDisplay.frame.size.width*scale, 
+                                         self.musicDisplay.frame.size.height*scale);
+    self.musicDisplay.center = CGPointMake(self.sheet.frame.size.width/2, 
+                                           self.sheet.frame.size.height-self.musicDisplay.frame.size.height+100);
+    [self.sheet addSubview:self.musicDisplay];
+    
+    [self.playPause removeTarget:nil action:NULL forControlEvents:UIControlEventTouchUpInside];
+    [self.playPause addTarget:self action:@selector(playHelpBackground) forControlEvents:UIControlEventTouchUpInside];
+}
+
+-(void)lesson {
+    [self.musicDisplay removeFromSuperview]; 
+    self.label.text = [NSString stringWithFormat:@"Press play to start!\n"
+                       "MetroGnome will count off two bars before beginning.\n\n"
+                       "Hint: Your first note is C."];
+    
+    [self.button removeFromSuperview];
+    [self.button setTitle:[NSString stringWithFormat:@"Finish"] 
+                 forState:UIControlStateNormal];
+    [self.button addTarget:self action:@selector(finish) forControlEvents:UIControlEventTouchUpInside];
+    [self.sheet addSubview:self.button];
+    
+    [self.playPause removeFromSuperview];
+    [self.playPause removeTarget:nil action:NULL forControlEvents:UIControlEventTouchUpInside];
+    self.playPause.center = CGPointMake(self.playPause.center.x, self.playPause.center.y-50);
+    [self.playPause setTitle:[NSString stringWithFormat:@"Play"] 
+                    forState:UIControlStateNormal];
+    [self.playPause setTitleColor:[UIColor blackColor] 
+                         forState:UIControlStateNormal];
+    [self.playPause addTarget:self action:@selector(playLesson) forControlEvents:UIControlEventTouchUpInside];
+    [self.sheet addSubview:self.playPause];
+    
+}
+
+-(void)finish {
+    [self pauseLesson];
+    [self go];
+}
+
+#pragma mark -
+#pragma BASS
+
+-(void)playBackground {
+    //play notes
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"C Major Blues Scale" ofType:@"mid"];  
+    HSTREAM stream=BASS_MIDI_StreamCreateFile(FALSE, [filePath UTF8String], 0, 0, 0, 44100);
+    if (BASS_ErrorGetCode()) {
+        NSLog(@"Bass error: %i", BASS_ErrorGetCode());
+    }
+    
+    MGInstrument *testInstrument = [[MGInstrument alloc]initWithSoundFont:nil instrumentType:nil];
+    
+    BASS_MIDI_FONT streamFont[1];
+    streamFont[0] = [testInstrument.soundFont getBASSMIDIFONT];
+    [testInstrument release];
+    
+    BASS_MIDI_StreamSetFonts(stream, streamFont, 1); // apply it to the stream
+    
+    //Enter into bass, but pause immediately
+    BASS_ChannelPlay(stream, FALSE);
+}
+
+-(void)playHelpBackground {
+    //play notes
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"Karios3Notes" ofType:@"mid"];  
+    HSTREAM stream=BASS_MIDI_StreamCreateFile(FALSE, [filePath UTF8String], 0, 0, 0, 44100);
+    if (BASS_ErrorGetCode()) {
+        NSLog(@"Bass error: %i", BASS_ErrorGetCode());
+    }
+    
+    MGInstrument *testInstrument = [[MGInstrument alloc]initWithSoundFont:nil instrumentType:nil];
+    
+    BASS_MIDI_FONT streamFont[1];
+    streamFont[0] = [testInstrument.soundFont getBASSMIDIFONT];
+    [testInstrument release];
+    
+    BASS_MIDI_StreamSetFonts(stream, streamFont, 1); // apply it to the stream
+    
+    //Play
+    BASS_ChannelPlay(stream, FALSE);
+}
+
+-(void)playLesson {     
     NSString *filePath = [[NSBundle mainBundle] pathForResource:@"Z Kairos Blues" ofType:@"mid"];  
     HSTREAM stream=BASS_MIDI_StreamCreateFile(FALSE, [filePath UTF8String], 0, 0, 0, 44100);
     if (BASS_ErrorGetCode()) {
@@ -76,8 +252,32 @@
     [testInstrument release];
     
     BASS_MIDI_StreamSetFonts(stream, streamFont, 1); // apply it to the stream
-    BASS_ChannelPlay(stream, FALSE);
+    
+    //Enter into bass, but pause immediately
+    BASS_ChannelPlay(stream, FALSE); 
+
+    [self.playPause removeTarget:nil action:NULL forControlEvents:UIControlEventTouchUpInside];
+    [self.playPause addTarget:self action:@selector(pauseLesson) forControlEvents:UIControlEventTouchUpInside];
+    [self.playPause setTitle:[NSString stringWithFormat:@"Pause"]
+                 forState:UIControlStateNormal];
 }
+
+-(void)pauseLesson {
+    BASS_Pause();
+    [self.playPause addTarget:self action:@selector(unpauseLesson) forControlEvents:UIControlEventTouchUpInside];
+    [self.playPause setTitle:[NSString stringWithFormat:@"Play"]
+                 forState:UIControlStateNormal];
+}
+
+-(void)unpauseLesson {
+    BASS_Start();
+    [self.playPause removeTarget:nil action:NULL forControlEvents:UIControlEventTouchUpInside];
+    [self.playPause addTarget:self action:@selector(pauseLesson) forControlEvents:UIControlEventTouchUpInside];
+    [self.playPause setTitle:[NSString stringWithFormat:@"Pause"]
+                    forState:UIControlStateNormal];
+}
+
+/****************************************************************************/
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
